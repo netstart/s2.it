685864658c5329543070f905022192fe37a59b06
Clayton K. N. Passos <netstart@gmail.com>
2018-05-28 19:01:33 -0300

    initial Question09

---
+++ questio08/question08/src/main/java/com/github/s2it/question09/Question09.java
@@ -0,0 +1,18 @@
+package com.github.s2it.question09;
+
+public class Question09 {
+	int valor;
+
+	Question09 left;
+	Question09 right;
+
+	public Question09(int valor) {
+		this.valor = valor;
+	}
+
+	public Question09 add(Question09 node) {
+
+		return this;
+	}
+
+}
