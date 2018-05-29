71f0125ba3279ca598063924627b67c2788332f7
Clayton K. N. Passos <netstart@gmail.com>
2018-05-29 16:39:46 -0300

    refactoring to clean question 8

--- questions/src/main/java/com/github/s2it/question08/Question08.java
+++ questions/src/main/java/com/github/s2it/question08/Question08.java
@@ -2,25 +2,38 @@ package com.github.s2it.question08;
 
 public class Question08 {
 
+	private String MAX = "1000000";
+
 	public String build(String a, String b) {
 		if (!existNumber(a) || !existNumber(b)) {
 			return "";
 		}
 
-		int sizeTotal = a.length() + b.length();
-
-		if (sizeTotal >= "1000000".length()) {
+		int sizeTotal = sumLength(a, b);
+		if (this.isGreatThanMax(sizeTotal)) {
 			return "-1";
 		}
 
-		StringBuilder c = new StringBuilder();
+		return spread(a, b);
+	}
 
+	private String spread(String a, String b) {
+		int sizeTotal = sumLength(a, b);
+
+		StringBuilder c = new StringBuilder();
 		for (int position = 0; position < sizeTotal; position++) {
 			appendCharAt(a, position, c);
 			appendCharAt(b, position, c);
 		}
-
 		return c.toString();
+	}
+
+	private boolean isGreatThanMax(int sizeTotal) {
+		return sizeTotal >= MAX.length();
+	}
+
+	private int sumLength(String a, String b) {
+		return a.length() + b.length();
 	}
 
 	private StringBuilder appendCharAt(String from, int position, StringBuilder to) {
